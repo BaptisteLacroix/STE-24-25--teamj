8cc61e0ba98ddb5c36025dec347a96585d00830d
Antoine Maïstre-Rice <antoinemaitre06@gmail.com>
2024-12-04 10:13:11 +0100

    add static header

---
+++ frontend/ste-24-25--teamj-frontend/src/components/header.tsx
@@ -0,0 +1,34 @@
+import { Button, Input, Navbar, NavbarContent } from "@nextui-org/react";
+import { SearchIcon } from "./searchIcon";
+
+export function PolyfoodHeader() {
+    return (
+        <Navbar isBordered>
+          <NavbarContent as="div" className="items-center flex" justify="start">
+            <Button radius="full">New Group Order</Button>
+            <Button radius="full">Join Group Order</Button>
+          </NavbarContent>
+    
+          <NavbarContent as="div" className="items-center" justify="center">
+            <Input
+              classNames={{
+                base: "max-w-full sm:max-w-[20rem] h-10",
+                mainWrapper: "h-full",
+                input: "text-small",
+                inputWrapper: "h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20",
+              }}
+              placeholder="Type to search..."
+              size="lg"
+              startContent={<SearchIcon size={18} />}
+              type="search"
+            />
+          </NavbarContent>
+          <NavbarContent as="div" className="items-center" justify="end">
+            <Button isIconOnly radius="full">
+              <img src="src/assets/shopping-cart.svg" alt="shopping-cart-icon" />
+            </Button>
+          </NavbarContent>
+        </Navbar>
+      );
+}
+
